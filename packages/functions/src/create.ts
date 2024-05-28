326065ef0203ebaf75575ab69f20dda90b8901f7
zhhot@hotmail.com <zhhot@hotmail.com>
2024-05-29 09:48:14 +1200

    Add an API to Create a Note

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,31 +1,56 @@
+import AWS from "aws-sdk";
 import * as uuid from "uuid";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
 import { Table } from "sst/node/table";
-import handler from "@notes/core/handler";
-import dynamoDb from "@notes/core/dynamodb";
+import { Console } from "console";
 
-export const main = handler(async (event) => {
-  let data = {
-    content: "",
-    attachment: "",
-  };
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  if (event.body != null) {
-    data = JSON.parse(event.body);
+export async function main(event: APIGatewayProxyEvent) {
+  let data, params;
+
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  if (event.body) {
+    console.log("aaa",event.body)
+    const jsonString = JSON.stringify(event.body);
+    console.log("bbb",jsonString)
+    data = JSON.parse(jsonString);
+    params = {
+      TableName: Table.Notes.tableName,
+      Item: {
+        // The attributes of the item to be created
+        userId: "123", // The id of the author
+        noteId: uuid.v1(), // A unique uuid
+        content: data.content, // Parsed from request body
+        attachment: data.attachment, // Parsed from request body
+        createdAt: Date.now(), // Current Unix timestamp
+      },
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: true }),
+    };
   }
 
-  const params = {
-    TableName: Table.Notes.tableName,
-    Item: {
-      // The attributes of the item to be created
-      userId: "123", // The id of the author
-      noteId: uuid.v1(), // A unique uuid
-      content: data.content, // Parsed from request body
-      attachment: data.attachment, // Parsed from request body
-      createdAt: Date.now(), // Current Unix timestamp
-    },
-  };
+  try {
+    await dynamoDb.put(params).promise();
 
-  await dynamoDb.put(params);
-
-  return JSON.stringify(params.Item);
-});
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (error) {
+    let message;
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = String(error);
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: message }),
+    };
+  }
+}
